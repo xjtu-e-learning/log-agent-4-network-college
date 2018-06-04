ac014ec371d09db2e84245273e39e4a44fb34a65
script <xjtushilei@foxmail.com>
2018-06-04 11:26:43 +0800

    增加权限认证

--- src/main/java/com/xjtushilei/logagent4networkcollege/controller/PostLogController.java
+++ src/main/java/com/xjtushilei/logagent4networkcollege/controller/PostLogController.java
@@ -2,18 +2,18 @@ package com.xjtushilei.logagent4networkcollege.controller;
 
 import com.xjtushilei.logagent4networkcollege.entity.ActionLog;
 import com.xjtushilei.logagent4networkcollege.entity.VisitLog;
-import com.xjtushilei.logagent4networkcollege.kafka.ConsumerService;
 import com.xjtushilei.logagent4networkcollege.kafka.ProducerService;
 import com.xjtushilei.logagent4networkcollege.utils.IpUtil;
 import com.xjtushilei.logagent4networkcollege.utils.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 import java.util.Random;
 
 /**
@@ -30,40 +30,50 @@ public class PostLogController {
     @Autowired
     ProducerService producerService;
 
+    @Value("${referrer-auth}")
+    String[] referrerAuth;
+
 
     @GetMapping("/hi")
-    public String hi(){
+    public String hi() {
         return "hi ! " + new Random().nextInt(100);
     }
 
     @PostMapping("/visit")
     public String visit(@RequestBody VisitLog visitLog, HttpServletRequest request) {
+        String referrer = request.getHeader("Verification-referrer");
+        if (Arrays.asList(referrerAuth).parallelStream().anyMatch(referrer::contains)) {
+            visitLog.setIp(IpUtil.getIpAddr(request));
+            visitLog.setUserAgent(request.getHeader("User-Agent"));
 
-        visitLog.setIp(IpUtil.getIpAddr(request));
-        visitLog.setUserAgent(request.getHeader("User-Agent"));
+            visitLog.setDate(new Date());
 
-        visitLog.setDate(new Date());
-
-        log.debug(visitLog.toString());
-        String json = JsonUtils.toJson(visitLog);
-        producerService.send("visit", json);
+            log.debug(visitLog.toString());
+            String json = JsonUtils.toJson(visitLog);
+            producerService.send("visit", json);
 //        consumerService.processVisitLogMessageByMongo(json);
 //        consumerService.processVisitLogMessageByElasticSearch(json);
-        return "visit log put success.";
+            return "visit log put success.";
+        } else {
+            return "visit log put failure. Authority authentication failure";
+        }
     }
 
     @PostMapping("/action")
-    public String action(@RequestBody ActionLog actionLog) {
-
-
-        actionLog.setDate(new Date());
-
-        log.debug(actionLog.toString());
-        String json = JsonUtils.toJson(actionLog);
-        producerService.send("action", json);
+    public String action(@RequestBody ActionLog actionLog, HttpServletRequest request) {
+        String referrer = request.getHeader("Verification-referrer");
+        if (Arrays.asList(referrerAuth).parallelStream().anyMatch(referrer::contains)) {
+            actionLog.setDate(new Date());
+            log.debug(actionLog.toString());
+            String json = JsonUtils.toJson(actionLog);
+            producerService.send("action", json);
 //        consumerService.processActionLogMessageByElasticSearch(json);
 //        consumerService.processActionLogMessageByMongo(json);
-        return "action log put success.";
+            return "action log put success.";
+        } else {
+            return "action log put failure. Authority authentication failure";
+        }
+
     }
 
 }
